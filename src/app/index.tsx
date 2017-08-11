dd97e775161fbf119b151d66570464ce6a8fb505
Erwin GO <erwingaitano@gmail.com>
2017-08-11 12:05:19 -0500

    preventing zooming in the page with pinch gesture

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,16 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+import { webFrame } from 'electron';
 import * as path from 'path';
 import * as React from 'react';
 import * as reactDOM from 'react-dom';
 import { App } from './_components/App';
 import './_css/index.scss';
 import './_fonts/icons/font';
+
+// Prevent zomming in the page with pinch gesture/shortcuts
+webFrame.setVisualZoomLevelLimits(1, 1);
 
 reactDOM.render(
   <App />,
