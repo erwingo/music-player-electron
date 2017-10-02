cd133df7f32c7ea3fe68fabbdb98f3741f329209
Erwin GO <erwingaitano@gmail.com>
2017-10-02 14:39:14 -0500

    use enzyme to test react components

--- src/renderer/_components/List/index.tests.tsx
+++ src/renderer/_components/List/index.tests.tsx
@@ -1,10 +1,16 @@
 import * as assert from 'assert';
+import { configure, shallow } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { List } from './';
 
+configure({ adapter: new Adapter() });
+
 describe('List', () => {
   it('renders without crashing', () => {
-    // ReactDOM.render(<List title='Noice' />, null);
+    assert.doesNotThrow(() => {
+      shallow(<List title='Noice' />);
+    });
   });
 });
